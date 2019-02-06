9a1788cdb55e65df75cfcb4662ab22dd3759bda5
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-02-06 16:36:58 +0000

    Removed Douglas from the database

--- mongo.py
+++ mongo.py
@@ -19,13 +19,7 @@ conn = mongo_connect(MONGODB_URI)
 #pass the connection name to a variable
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 
-    'gender': 'm', 'hair_colour': 'not much', 'occupation': 'writer',
-    'nationality': 'english'}, {'first': 'george', 'last': 'rr martin',
-    'dob': '20/09/1948', 'gender': 'm', 'hair_colour': 'white',
-    'occupation': 'writer', 'nationality': 'american'}]
-
-coll.insert_many(new_docs)
+coll.remove({'first': 'douglas'})
 
 documents = coll.find()
 
