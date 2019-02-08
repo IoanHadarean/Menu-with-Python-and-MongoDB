2890f8894af0808a8049c1b21eeedcd587a42ba6
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-02-08 14:15:19 +0000

    Added get_record function

--- mongo_project.py
+++ mongo_project.py
@@ -24,6 +24,22 @@ def show_menu():
     option = input("Enter option: ")
     return option
     
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    
+    try:
+       doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+    
+    if not doc:
+        print("")
+        print("Error! No results found.")
+        
+    return doc
+    
 def add_record():
     print("")
     first = input("Enter first name > ")
