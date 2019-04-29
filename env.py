78b3318f8cd27d8a08b5d306fdc040a3a7a19ec9
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-29 08:09:41 +0000

    Removed cached env

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-#storing the MONGO_URI environment variable, which will be exported to mongo_project.py
-os.environ["MONGO_URI"]= "mongodb://root:1403Goagl@ds015774.mlab.com:15774/mytestdb"
